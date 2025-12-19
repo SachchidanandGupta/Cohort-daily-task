510c72a42870a8a000ade2f651171db4a90405b9
Sachchidanand Gupta <sachchidanandgupta2006@gmail.com>
2025-12-19 15:12:42 +0530

    complete text transformation effect

--- TextChangeEffect/script.js
+++ TextChangeEffect/script.js
@@ -1,17 +1,31 @@
-var para = document.querySelector('p');
+var para = document.querySelector("p");
 var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 var text = para.innerText;
-para.addEventListener("mouseenter",()=>{
-  var set =  setInterval(()=>{
- var str = text.split('').map((char,idx)=>{
-           return characters.split('')[Math.floor(Math.random()*53)]
+
+// function randomText() {
+//   const str = text.split("").map((char, idx) => {
+//     if(iteration > idx){
+//         return char;
+//     }
+//     return characters.split('')[Math.floor(Math.random()*52)];
+//   }).join('');
+//   para.innerText = str;
+//   iteration += 0.2;
+// }
+// setInterval(randomText,30);
+
+var iteration = 0;
+para.addEventListener("mouseenter",function randomText(){
+  var str =  text.split('').map((char,idx)=>{
+    if(idx < iteration){
+        return char;
+    }
+       return characters.split('')[Math.floor(Math.random() * characters.length)];
     }).join('');
-       para.innerHTML = str;
-    },10)
-    setTimeout(()=>{
-        clearInterval(set);
-        para.innerHTML = text
-    },200)
-   
+    para.innerText = str;
+     iteration += 0.25;
+     setInterval(randomText,50)
     
-})
+});
+
+ 
